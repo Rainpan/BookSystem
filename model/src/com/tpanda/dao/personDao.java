8f0762baed35d1374dd5fe189ac7d7105f387710
rain <624995915@qq.com>
2016-09-05 15:52:32 +0800

    commit file

--- model/src/com/tpanda/dao/personDao.java
+++
@@ -1,8 +0,0 @@
-package com.tpanda.dao;
-
-
-public interface PersonDao {
-
-    public boolean login(String name,String pwd);
-
-}
---
+++ model/src/com/tpanda/entity/class_student.java
@@ -0,0 +1,66 @@
+package com.tpanda.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class class_student {
+    private int csId;
+    private int claId;
+    private int stuId;
+    private int score;
+    private Date scoreDate;
+
+    @Id
+    @Column(name = "cs_id")
+    @GenericGenerator(name = "dir",strategy = "increment")
+    @GeneratedValue(generator = "dir")
+    public int getCsId() {
+        return csId;
+    }
+
+    public void setCsId(int csId) {
+        this.csId = csId;
+    }
+
+    @Column(name = "cla_id")
+    public int getClaId() {
+        return claId;
+    }
+
+    public void setClaId(int claId) {
+        this.claId = claId;
+    }
+
+    @Column(name = "stu_id")
+    public int getStuId() {
+        return stuId;
+    }
+
+    public void setStuId(int stuId) {
+        this.stuId = stuId;
+    }
+
+    @Column(name = "score")
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Column(name = "score_date")
+    public Date getScoreDate() {
+        return scoreDate;
+    }
+
+    public void setScoreDate(Date scoreDate) {
+        this.scoreDate = scoreDate;
+    }
+}
